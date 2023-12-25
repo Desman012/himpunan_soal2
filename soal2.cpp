cc5ea72a9405763d09ab051737a7099aed1a124b
Desman Dwi Saputra <desmandwisaputra@gmail.com>
2023-12-25 22:39:25 +0700

    Add files via upload

---
+++ soal2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int base = 10;
+    int height = 20;
+
+    cout << "base of the triangle " << base << endl;
+    cout << "height of the triangle " << height << endl;
+
+    int total = 0.5 * base * height;
+    cout << "Area of the triangle " << total; 
+}
